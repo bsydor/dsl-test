0e6f6930a431116bcc720463d24cc7ba262a12c3
Bohdan Sydor <bsydor@gmail.com>
2019-01-16 21:45:56 +0100

    Restructed git block

--- jobs/selenium.groovy
+++ jobs/selenium.groovy
@@ -2,8 +2,11 @@ pipelineJob('selenium') {
     definition {
         cpsScm {
             scm {
-                git('https://github.com/AVORA-LTD/selenium.git')
-                credentialsId('github-clone')
+                git {
+                  branch('test-jenkins')
+                  remote('https://github.com/AVORA-LTD/selenium.git')
+                  credentialsId('github-clone')
+                }
                 scriptPath('JenkinsFile')
             }
         }
